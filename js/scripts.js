21d4afd77ec40098dbd68167f8b12106d179cc39
Akjol Jaenbai and Brian Nelson <brian@>
2018-08-07 15:34:52 -0700

    add styles to webpage. combine scripts.js click functions into 1 universal click function.

--- js/scripts.js
+++ js/scripts.js
@@ -1,86 +1,91 @@
 $(document).ready(function() {
 
-  $("#term .java").click(function() {
+  $("#term ul li").click(function() {
     $("#definition ul li").hide();
-    $("#definition .java").toggle();
+    $("#definition ." + $(this).attr("class")).toggle();
+  });
 
-  });
-  $("#term .operators").click(function() {
-    $("#definition ul li").hide();
-    $("#definition .operators").toggle();
-  });
-  $("#term .variable").click(function() {
-    $("#definition ul li").hide();
-    $("#definition .variable").toggle();
-  });
-  $("#term .variable-name").click(function() {
-    $("#definition ul li").hide();
-    $("#definition .variable-name").toggle();
-  });
-  $("#term .conventions").click(function() {
-    $("#definition ul li").hide();
-    $("#definition .conventions").toggle();
-  });
-  $("#term .functions").click(function() {
-    $("#definition ul li").hide();
-    $("#definition .functions").toggle();
-  });
-  $("#term .methods").click(function() {
-    $("#definition ul li").hide();
-    $("#definition .methods").toggle();
-  });
-  $("#term .arguments").click(function() {
-    $("#definition ul li").hide();
-    $("#definition .arguments").toggle();
-  });
-  $("#term .parameters").click(function() {
-    $("#definition ul li").hide();
-    $("#definition .parameters").toggle();
-  });
-  $("#term .return").click(function() {
-    $("#definition ul li").hide();
-    $("#definition .return").toggle();
-  });
-  $("#term .chaining-methods").click(function() {
-    $("#definition ul li").hide();
-    $("#definition .chaining-methods").toggle();
-  });
-  $("#term .strings").click(function() {
-    $("#definition ul li").hide();
-    $("#definition .strings").toggle();
-  });
-  $("#term .booleans").click(function() {
-    $("#definition ul li").hide();
-    $("#definition .booleans").toggle();
-  });
-  $("#term .undefined").click(function() {
-    $("#definition ul li").hide();
-    $("#definition .undefined").toggle();
-  });
-  $("#term .nan").click(function() {
-    $("#definition ul li").hide();
-    $("#definition .nan").toggle();
-  });
-  $("#term .escape").click(function() {
-    $("#definition ul li").hide();
-    $("#definition .escape").toggle();
-  });
-  $("#term .alert1").click(function() {
-    $("#definition ul li").hide();
-    $("#definition .alert1").toggle();
-  });
-  $("#term .comments").click(function() {
-    $("#definition ul li").hide();
-    $("#definition .comments").toggle();
-  });
-  $("#term .jQuery").click(function() {
-    $("#definition ul li").hide();
-    $("#definition .jQuery").toggle();
-  });
-  $("#term .attributes").click(function() {
-    $("#definition ul li").hide();
-    $("#definition .attributes").toggle();
-  });
+  // $("#term .java").click(function() {
+  //   $("#definition ul li").hide();
+  //   $("#definition .java").toggle();
+  //
+  // });
+  // $("#term .operators").click(function() {
+  //   $("#definition ul li").hide();
+  //   $("#definition .operators").toggle();
+  // });
+  // $("#term .variable").click(function() {
+  //   $("#definition ul li").hide();
+  //   $("#definition .variable").toggle();
+  // });
+  // $("#term .variable-name").click(function() {
+  //   $("#definition ul li").hide();
+  //   $("#definition .variable-name").toggle();
+  // });
+  // $("#term .conventions").click(function() {
+  //   $("#definition ul li").hide();
+  //   $("#definition .conventions").toggle();
+  // });
+  // $("#term .functions").click(function() {
+  //   $("#definition ul li").hide();
+  //   $("#definition .functions").toggle();
+  // });
+  // $("#term .methods").click(function() {
+  //   $("#definition ul li").hide();
+  //   $("#definition .methods").toggle();
+  // });
+  // $("#term .arguments").click(function() {
+  //   $("#definition ul li").hide();
+  //   $("#definition .arguments").toggle();
+  // });
+  // $("#term .parameters").click(function() {
+  //   $("#definition ul li").hide();
+  //   $("#definition .parameters").toggle();
+  // });
+  // $("#term .return").click(function() {
+  //   $("#definition ul li").hide();
+  //   $("#definition .return").toggle();
+  // });
+  // $("#term .chaining-methods").click(function() {
+  //   $("#definition ul li").hide();
+  //   $("#definition .chaining-methods").toggle();
+  // });
+  // $("#term .strings").click(function() {
+  //   $("#definition ul li").hide();
+  //   $("#definition .strings").toggle();
+  // });
+  // $("#term .booleans").click(function() {
+  //   $("#definition ul li").hide();
+  //   $("#definition .booleans").toggle();
+  // });
+  // $("#term .undefined").click(function() {
+  //   $("#definition ul li").hide();
+  //   $("#definition .undefined").toggle();
+  // });
+  // $("#term .nan").click(function() {
+  //   $("#definition ul li").hide();
+  //   $("#definition .nan").toggle();
+  // });
+  // $("#term .escape").click(function() {
+  //   $("#definition ul li").hide();
+  //   $("#definition .escape").toggle();
+  // });
+  // $("#term .alert1").click(function() {
+  //   $("#definition ul li").hide();
+  //   $("#definition .alert1").toggle();
+  // });
+  // $("#term .comments").click(function() {
+  //   $("#definition ul li").hide();
+  //   $("#definition .comments").toggle();
+  // });
+  // $("#term .jQuery").click(function() {
+  //   $("#definition ul li").hide();
+  //   $("#definition .jQuery").toggle();
+  // });
+  // $("#term .attributes").click(function() {
+  //   $("#definition ul li").hide();
+  //   $("#definition .attributes").toggle();
+  // });
 
 
 
