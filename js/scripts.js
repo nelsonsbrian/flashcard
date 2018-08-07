6708f4c7f69f2a90994d65a61103da2fe2822d71
Akjol Jaenbai and Brian Nelson <brian@>
2018-08-07 14:00:05 -0700

    add terms and definition. allow definitions to appear when the term is clicked upon

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,92 @@
 $(document).ready(function() {
-  $("button#bark").click(function() {
-    // $('ul#dog').prepend('<img class="ruff" src="img/dog.png" />');
-    $("ul#dog").prepend("<li>Bark!</li>");
-    // $('ul#cat').prepend('<img class="meow" src="img/cat.jpeg" />');
-    $("ul#cat").prepend("<li>meooww?</li>");
+
+  $("#term .java").click(function() {
+    $("#definition ul li").hide();
+    $("#definition .java").toggle();
+
+  });
+  $("#term .operators").click(function() {
+    $("#definition ul li").hide();
+    $("#definition .operators").toggle();
+  });
+  $("#term .variable").click(function() {
+    $("#definition ul li").hide();
+    $("#definition .variable").toggle();
+  });
+  $("#term .variable-name").click(function() {
+    $("#definition ul li").hide();
+    $("#definition .variable-name").toggle();
+  });
+  $("#term .conventions").click(function() {
+    $("#definition ul li").hide();
+    $("#definition .conventions").toggle();
+  });
+  $("#term .functions").click(function() {
+    $("#definition ul li").hide();
+    $("#definition .functions").toggle();
+  });
+  $("#term .methods").click(function() {
+    $("#definition ul li").hide();
+    $("#definition .methods").toggle();
+  });
+  $("#term .arguments").click(function() {
+    $("#definition ul li").hide();
+    $("#definition .arguments").toggle();
+  });
+  $("#term .parameters").click(function() {
+    $("#definition ul li").hide();
+    $("#definition .parameters").toggle();
+  });
+  $("#term .return").click(function() {
+    $("#definition ul li").hide();
+    $("#definition .return").toggle();
+  });
+  $("#term .chaining-methods").click(function() {
+    $("#definition ul li").hide();
+    $("#definition .chaining-methods").toggle();
+  });
+  $("#term .strings").click(function() {
+    $("#definition ul li").hide();
+    $("#definition .strings").toggle();
+  });
+  $("#term .booleans").click(function() {
+    $("#definition ul li").hide();
+    $("#definition .booleans").toggle();
+  });
+  $("#term .undefined").click(function() {
+    $("#definition ul li").hide();
+    $("#definition .undefined").toggle();
+  });
+  $("#term .nan").click(function() {
+    $("#definition ul li").hide();
+    $("#definition .nan").toggle();
+  });
+  $("#term .escape").click(function() {
+    $("#definition ul li").hide();
+    $("#definition .escape").toggle();
+  });
+  $("#term .alert1").click(function() {
+    $("#definition ul li").hide();
+    $("#definition .alert1").toggle();
+  });
+  $("#term .comments").click(function() {
+    $("#definition ul li").hide();
+    $("#definition .comments").toggle();
+  });
+  $("#term .jQuery").click(function() {
+    $("#definition ul li").hide();
+    $("#definition .jQuery").toggle();
+  });
+  $("#term .attributes").click(function() {
+    $("#definition ul li").hide();
+    $("#definition .attributes").toggle();
   });
 
-  $("button#meow").click(function() {
-    // $('ul#cat').prepend('<img class="meow" src="img/cat.jpeg" />');
-    $("ul#cat").prepend("<li>Meow!</li>");
-    // $('ul#dog').prepend('<img class="ruff" src="img/dog.png" />');
-    $("ul#dog").prepend("<li>Ruff?</li>");
-    $("ul#cat").click(function() {
-    alert('hi');
-  });
-  // $("img").click(function(){
-  //   $(this).remove();
-  // });
 
 
 });
 
-//   $(this).remove();
-// });
-// $("ul.ruff").children("li").first().click(function() {
-//   $(this).remove();
-// });
+
+
+
+    // $(this).children("")
